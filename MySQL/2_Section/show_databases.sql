968f3c99a722faf056df470b7aac66cea04e8f51
Mukhammadkodir Abdusalomov <99057791+Mukhammadkodir27@users.noreply.github.com>
2024-11-01 23:52:46 +0100

    show_create_database

---
+++ MySQL/2_Section/show_databases.sql
@@ -0,0 +1,13 @@
+show databases;
+
+-- when you install MySQL, there will be some default databases.
+
+-- in terminal you can write > mysql -u root -p
+-- then you enter your password
+
+-- Creating databases
+CREATE DATABASE example_database;
+
+-- Removing a database entirely with its contents
+DROP DATABASE example_database;
+
